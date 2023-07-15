40449d257760f7d6009d5a141ca9c1750eda46ff
Aaron Zhang <aarondzhang33@gmail.com>
2023-07-15 15:18:59 -0700

    beta v0.3 heroku

--- src/main/java/com/dofu2/config/util/Constants.java
+++ src/main/java/com/dofu2/config/util/Constants.java
@@ -5,7 +5,6 @@ public class Constants {
     public static final String DF_YYMMDD = "yyMMdd";
     public static final String DF_YYYY_MM_DD = "yyyy-MM-dd";
     public static final String DF_YYYY_MM_DD_HH_MM = "yyyy-MM-dd HH:mm:ss";
-    public static final String DF_YYYYMMDDHHmmssSSS = "yyyyMMddHHmmssSSS";
 
     public static final String DefaultDateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     //2023-07-04T03:28:39.731Z
